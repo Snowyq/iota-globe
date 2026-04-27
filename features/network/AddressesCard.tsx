084809524d9d1b00fbe6cf7c4b7269b1c1bd0d90
Jakub Pniewski <jakub.pniewski@icloud.com>
2026-04-27 20:00:00 +0200

    fix styling

--- features/network/AddressesCard.tsx
+++ features/network/AddressesCard.tsx
@@ -10,19 +10,35 @@ import { useContext } from "react";
 export function AddressesCard({ className }: { className?: string }) {
     const { addressMetrics } = useContext(NetworkContext);
 
-    const total = formatNumber(addressMetrics ? Number(addressMetrics.cumulativeAddresses) : null);
-    const totalActive = formatNumber(addressMetrics ? Number(addressMetrics.cumulativeActiveAddresses) : null);
-    const dailyActive = formatNumber(addressMetrics ? Number(addressMetrics.dailyActiveAddresses) : null);
+    const total = formatNumber(
+        addressMetrics ? Number(addressMetrics.cumulativeAddresses) : null
+    );
+    const totalActive = formatNumber(
+        addressMetrics ? Number(addressMetrics.cumulativeActiveAddresses) : null
+    );
+    const dailyActive = formatNumber(
+        addressMetrics ? Number(addressMetrics.dailyActiveAddresses) : null
+    );
 
     return (
         <Card className={cn(className)}>
             <CardHeader>
                 <CardTitle>Addresses</CardTitle>
             </CardHeader>
-            <CardContent className="grid grid-cols-3 gap-4">
+            <CardContent className="grid grid-cols-3 items-end gap-4">
                 <Stat value={total.value} sub={total.label} label="Total" />
-                <Stat value={totalActive.value} sub={totalActive.label} label="Total Active" size="sm" />
-                <Stat value={dailyActive.value} sub={dailyActive.label} label="Daily Active" size="sm" />
+                <Stat
+                    value={totalActive.value}
+                    sub={totalActive.label}
+                    label="Total Active"
+                    size="sm"
+                />
+                <Stat
+                    value={dailyActive.value}
+                    sub={dailyActive.label}
+                    label="Daily Active"
+                    size="sm"
+                />
             </CardContent>
         </Card>
     );
